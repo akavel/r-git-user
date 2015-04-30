8f4a6fd55b6de70b194e8f48303151f02c934ff9
Mateusz Czapliński <czapkofan@gmail.com>
2015-04-30 23:17:16 +0200

    rename funcs and make them take optional 'global' param

--- R/git.user.R
+++ R/git.user.R
@@ -1,23 +1,36 @@
-git.get.user.global <- function() {
-  name <- system2('git', args=c('config', '--get', 'user.name'), stdout=TRUE)
+git.get.user <- function(global=FALSE) {
+  opt_global <- c('--local')
+  if (global) {
+    opt_global <- c('--global')
+  }
+  
+  name <- system2('git', args=c('config', opt_global, '--get', 'user.name'), stdout=TRUE)
   if (!is.null(attr(name, 'status'))) {
     stop("error running git")
   }
   # TODO: worked for me on windows; make sure on Linux too
   Encoding(name) <- 'UTF-8'
   
-  email <- system2('git', args=c('config', '--get', 'user.email'), stdout=TRUE)
+  email <- system2('git', args=c('config', opt_global, '--get', 'user.email'), stdout=TRUE)
   if (!is.null(attr(email, 'status'))) {
     stop("error running git")
   }
   return(list(name=name, email=email))
 }
 
-git.set.user.global <- function(name, email) {
+git.set.user <- function(name, email, global=FALSE) {
+  is.character(name) || stop('name must be text, e.g. "John Smith"')
+  is.character(email) || stop('email must be text, e.g. "jsmith@example.com"')
+  
+  opt_global <- c('--local')
+  if (global) {
+    opt_global <- c('--global')
+  }
+  
   # TODO: quoting works on windows, but test on Linux too
   # TODO: make sure it works for double quoted: e.g. 'John "Rocket" Smith'
   name <- paste('"', name, '"', sep='')
-  out <- system2('git', args=c('config', '--global', '--replace-all', 'user.name', name), stdout=TRUE)
+  out <- system2('git', args=c('config', opt_global, '--replace-all', 'user.name', name), stdout=TRUE)
   if (!is.null(attr(out, 'status'))) {
     stop("error running git")
   }
@@ -25,10 +38,9 @@ git.set.user.global <- function(name, email) {
   # TODO: quoting works on windows, but test on Linux too
   # TODO: make sure it works for double quoted: e.g. 'John "Rocket" Smith'
   email <- paste('"', email, '"', sep='')
-  out <- system2('git', args=c('config', '--global', '--replace-all', 'user.email', email), stdout=TRUE)
+  out <- system2('git', args=c('config', opt_global, '--replace-all', 'user.email', email), stdout=TRUE)
   if (!is.null(attr(out, 'status'))) {
     stop("error running git")
   }
 }
 
-# TODO: add default local versions of the commands, with optional 'global=TRUE' parameter
